72b3b60f690804f8fa30de00c0f683c5d9d4f968
paithankarashwini <paithankarahwini1@gmail.com>
2024-10-18 14:07:14 +0530

    test added

---
+++ src/main/java/com/Test/DemoTest.java
@@ -0,0 +1,14 @@
+package com.Test;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		
+		String s="Hello";
+		int length =s.length();
+		
+		System.out.println(length);
+
+	}
+
+}
